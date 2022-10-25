e1bb0d2b3fe0df7716d43e1fad2247754dc26a75
humayonitbd <humayonforid44@gmail.com>
2022-10-25 15:27:04 +0600

    api data

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const category = require('./data/category.json');
+const data = require('./data/data.json')
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running...')
+})
+
+app.get('/category', (req, res)=>{
+    res.send(category)
+})
+
+app.get('/data', (req, res)=>{
+    res.send(data)
+})
+
+app.get('/data/:id',(req, res)=>{
+    const id = parseInt(req.params.id);
+    // console.log(req.params.id)
+    const dataId = data.find(dt =>dt.id === id);
+    res.send(dataId)
+})
+
+
+
+app.listen(port,()=>{
+    console.log('your port is', port)
+})
